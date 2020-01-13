7fe8079d226c60a9f50e2a1368cd5f81c0dd9ed8
hama1224 <hama.k1224@gmail.com>
2020-01-13 10:35:34 +0000

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 15}
     validates :email, presence: true
-    validates :email, format: {with: /\A\S+@\S+\.\S+\z/i}
+    validates :email, format: {with: /\A[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\Z/}
     
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
     
     has_secure_password
     
     has_many :topics
+    has_many :favorites
+    has_many :favorite_topics, through: :favorites, source: 'topic'
 end
